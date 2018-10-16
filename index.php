93646b2d70485ca4da6002a3be7fdd0ccec0a408
smr2vii <smr2vii@samirs-mbp.lan>
2018-10-16 13:51:23 +0300

    html

--- index.php
+++ index.php
@@ -1,15 +1,21 @@
+<?php 
+ print date('Y-m-d H:i:s');
+?>
+
 <html>
+    
     <head>
-    <title>As valdau HTML</title>
+        <title> As valdau HTML<?php print "ir PHP";?> ! </title>
     </head>
     
     <body>
     
-    <h1>Vardas - HTML asas/e!</h1>
-    
-    <p>Moku kurti statini turini.</p>
-    
-    
+        <h1>Vardas - HTML ir <?php print "ir PHP";?> asas/e! </h1>
+        <p>Moku kurti statini turini. Atidaryta: <?php print date('Y-m-d H:i:s');?></p>
     
     </body>
+    
 </html>
+
+
+ 
