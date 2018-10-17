1078b58ce5d7094078d0e4c94a1100dc454fa061
smr2vii <smr2vii@samirs-mbp.lan>
2018-10-17 18:36:09 +0300

    html

--- index.php
+++ index.php
@@ -1,36 +1,57 @@
-<?php 
+<?php
+$sunny = rand(0, 1);
+$rain = rand(0, 1);
 
+$class = null;
+$tekstas = null;
+
+
+if ($sunny) {    
+    if ($rain) {    
+        $class = 'sunny-rain';
+        $tekstas = 'Sauleta ir lyja';
+    } else {
+        $class = 'sunny';
+        $tekstas = 'Sauleta';
+    }
+} else {
+    if ($rain){  
+        $class = 'cloudy-rain';
+        $tekstas = 'Debesuota ir lyja';
+    } else {    
+        $class = 'cloudy';
+        $tekstas = 'Debesuota';
+    }
+}
 
 ?>
 
+<!DOCTYPE html>
 <html>
-
     <head>
-        
-        <title> PHP! </title>
+        <title>if</title>
         <style>
-            body {
-                background: rgb(<?php print rand(1, 255) . ',' . rand(1, 255) . ',' .rand(1, 255); ?>)
-                    
+            .paveikslelis {
+                background-size: cover;
+                width: 100px;
+                height: 100px;
             }
-            
-            h1{
-                font-size: <?php print rand(20, 50) ?>
+            .sunny{
+               background-image: url("sunny.png") 
             }
-            
-            p{
-                color: rgb(<?php print rand(1,255) . ',' . rand(1, 255) . ',' . rand(1, 255); ?>)
+            .sunny-rain{
+                background-image: url("rainy.png")
+            }
+            .cloudy{
+                background-image: url("sunny.png")
+            }
+            .cloudy-rain{
+                background-image: url("rainy.png")
             }
         </style>
     </head>
-    
     <body>
-        <h1> As keiciu dydi!</h1>
-        <p> As keiciu spalva!</p>
+        <div class="paveikslelis <?php print $class;?>"></div>
+        <p><?php print $tekstas;?></p>
     </body>
-    
-
 </html>
-
-
-
