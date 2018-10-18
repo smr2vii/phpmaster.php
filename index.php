b58f79d48296c7a69740b9d8f287d504b0f610b1
smr2vii <smr2vii@samirs-mbp.lan>
2018-10-18 15:01:06 +0300

    html

--- index.php
+++ index.php
@@ -1,27 +1,21 @@
 <?php
-$grizai_velai = rand(0, 1);
-$grizai_isgeres = rand(0, 1);
-
+$kiek_isgerei = rand(1, 8);
+$barnio_riba = rand(1, 4);
+$bokalai = $kiek_isgerei - $barnio_riba;
+$galejai = ($kiek_isgerei - 4) * -1;
 
         
-if ($grizai_velai && $grizai_isgeres) {
-     $f = "Miegosi and sofos,nes grizai vielai ir isgeres.";
-} else if ($grizai_isgeres) {
-     $f = "Nemiegosi ant sofos nes negrizai velai.";
-} else if ($grizai_velai) {
-     $f = "Nemiegosi and sofos nes negrizai isgeres.";
-} else {
-    $f = "nieko";
+if ($kiek_isgerei > 4) {
+     $bokalai = "Zmona barsis, nes isgerei $bokalai bokalais per daug.";
+}  else {
+    $bokalai = "Zmona nesibars. Siandien galejai isgerti dar $galejai bokalus.";
 }
 ?>
 
 
 <!DOCTYPE html>
 <html>
-    <head>
-        <title></title>
-    </head>
     <body>
-        <div> <?php print $f;?></div>
+        <div> <?php print $bokalai;?></div>
     </body>
 </html>
